99e43e62e131261a05bea3afd3805338e7a3304c
Lucas Verdi <lucas.a.verdi@gmail.com>
2026-09-16 16:01:14 -0300

    fix(manutencao): avoid 500 and return default payload

--- app/controller/manutencao.js
+++ app/controller/manutencao.js
@@ -7,10 +7,18 @@ module.exports = (app) => {
         try {
             let manutencao = await manutencaoModel.manutencao.findOne({});
 
-            res.send(crypto.encrypt(manutencao, true))
+            if (!manutencao) {
+                manutencao = await manutencaoModel.manutencao.create({
+                    manutencao: 0,
+                    msg_manutencao: ''
+                });
+            }
+
+            const payload = manutencao?.dataValues || manutencao;
+            res.send(crypto.encrypt(payload, true));
         } catch (error) {
             console.log(error);
-            res.status(500).send()
+            res.status(500).send();
         }
     }
 
